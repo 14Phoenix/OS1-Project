5de0478eb1b1b9fe1199f4d6528ca09f6879e9cb
Viktor <viktormitrovic00@gmail.com>
2023-08-03 20:06:12 +0200

    Created Scheduler.hpp and Scheduler.cpp files.

---
+++ h/Scheduler.hpp
@@ -0,0 +1,18 @@
+#ifndef PROJECT_BASE_H_SCHEDULER_H
+#define PROJECT_BASE_H_SCHEDULER_H
+
+
+#include "../lib/hw.h"
+
+
+class Scheduler {
+public:
+
+
+private:
+
+
+};
+
+
+#endif // PROJECT_BASE_H_SCHEDULER_H
---
+++ src/Scheduler.cpp
@@ -0,0 +1,1 @@
+#include "../h/Scheduler.hpp"
