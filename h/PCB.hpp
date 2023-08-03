31ce30176ef91b698601b6a6617b68ff529093ea
Viktor <viktormitrovic00@gmail.com>
2023-08-03 22:15:55 +0200

    Added createPCB, isFinished, setFinished, constructor, PCB::new, PCB::delete, dispatch and contextSwitch.

--- h/PCB.hpp
+++ h/PCB.hpp
@@ -3,14 +3,59 @@
 
 
 #include "../lib/hw.h"
+#include "./MemoryAllocator.hpp"
+#include "./Scheduler.hpp"
 
 
 class PCB {
 public:
+    using Body = void (*)(void*);
 
+    static PCB* createPCB(Body body, void *arg, void* stack);
+
+    bool isFinished() const;
+
+    void setFinished(bool finished);
+
+    static void yield();
+
+    static PCB *running;
 
 private:
+    PCB(Body body, void *arg, void* stack) :
+    body(body),
+    arg(arg),
+    stack(body != nullptr ? stack : nullptr),
+    context({
+        (uint64) &threadWrapper,
+        stack != nullptr ? (uint64) &(((uint8*)stack)[DEFAULT_STACK_SIZE]) : 0
+    }) {
+        if (body != nullptr) Scheduler::put(this);
+    }
 
+    void* operator new(size_t size);
+
+    void operator delete(void *ptr);
+
+    struct Context {
+        uint64 ra;
+        uint64 sp;
+    };
+
+    Body body;
+    void *arg;
+    void *stack;
+    Context context;
+
+    bool finished;
+
+    static void threadWrapper();
+
+    static void contextSwitch(Context *oldContext, Context *runningContext);
+
+    static void dispatch();
+
+    static uint64 timeSliceCounter;
 
 };
 
--- src/PCB.cpp
+++ src/PCB.cpp
@@ -1,2 +1,48 @@
 #include "../h/PCB.hpp"
 
+PCB *PCB::running = nullptr;
+
+uint64 PCB::timeSliceCounter = 0;
+
+PCB *PCB::createPCB(PCB::Body body, void *arg, void *stack) {
+//    size_t sizeToAllocate = (sizeof(PCB) / MEM_BLOCK_SIZE + (sizeof(PCB) % MEM_BLOCK_SIZE > 0 ? 1 : 0)) * MEM_BLOCK_SIZE;
+//    PCB *pcb = (PCB*) MemoryAllocator::getInstance()->memAlloc(sizeToAllocate);
+    PCB *pcb = new PCB(body, arg, stack);
+    return pcb;
+}
+
+bool PCB::isFinished() const {
+    return finished;
+}
+
+void PCB::setFinished(bool finished) {
+    this->finished = finished;
+}
+
+void PCB::yield() {
+    // add a0 code for dispatch for handleSupervisorTrap
+    // __asm__ volatile ("ecall");
+}
+
+void *PCB::operator new(size_t size) {
+    size_t sizeToAllocate = (sizeof(PCB) / MEM_BLOCK_SIZE + (sizeof(PCB) % MEM_BLOCK_SIZE > 0 ? 1 : 0)) * MEM_BLOCK_SIZE;
+    return MemoryAllocator::getInstance()->memAlloc(sizeToAllocate);
+}
+
+void PCB::operator delete(void *ptr) {
+    MemoryAllocator::getInstance()->memFree(ptr);
+}
+
+void PCB::threadWrapper() {
+    // sret here
+    running->body(running->arg);
+    running->setFinished(true);
+    yield();
+}
+
+void PCB::dispatch() {
+    PCB *oldPCB = running;
+    if (!oldPCB->isFinished()) Scheduler::put(oldPCB);
+    running = Scheduler::get();
+    contextSwitch(&oldPCB->context, &running->context);
+}
