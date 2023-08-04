3abfe479ae06efc728395c4fe01448f3269e83ff
Viktor <viktormitrovic00@gmail.com>
2023-08-04 20:58:34 +0200

    Added RiscV inline methods and popSppSpie.

--- src/RiscV.cpp
+++ src/RiscV.cpp
@@ -2,13 +2,17 @@
 
 #include "../lib/console.h"
 
+void RiscV::popSppSpie() {
+    __asm__ volatile ("csrw sepc, ra");
+    __asm__ volatile ("sret");
+}
+
 void RiscV::handleSupervisorTrap() {
 
     uint64 scause;
     __asm__ volatile ("csrr %[scause], scause" : [scause] "=r" (scause));
 
     if (scause == 0x8000000000000009UL) {
-
         console_handler();
     } else {
         // default
