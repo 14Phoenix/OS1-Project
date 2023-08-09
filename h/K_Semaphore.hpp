1b36b88e0eed92bd20f75dbfbc3b454148d770f9
Viktor <viktormitrovic00@gmail.com>
2023-08-09 19:59:43 +0200

    Added K_Semaphore.hpp and K_Semaphore.cpp.

---
+++ h/K_Semaphore.hpp
@@ -0,0 +1,18 @@
+#ifndef PROJECT_BASE_H_K_SEMAPHORE_H
+#define PROJECT_BASE_H_K_SEMAPHORE_H
+
+
+#include "../lib/hw.h"
+
+
+class K_Semaphore {
+public:
+
+
+private:
+
+
+};
+
+
+#endif // PROJECT_BASE_H_K_SEMAPHORE_H
