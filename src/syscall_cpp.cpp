0470e4ab0818264e71336787dcdb4672077d4485
Viktor <viktormitrovic00@gmail.com>
2023-08-14 02:56:10 +0200

    Added C++ API.

---
+++ src/syscall_cpp.cpp
@@ -0,0 +1,85 @@
+#include "../h/syscall_cpp.hpp"
+
+void* operator new (size_t size) {
+    return mem_alloc(size);
+}
+
+void operator delete (void *ptr) {
+    mem_free(ptr);
+}
+
+Thread::Thread(void (*body)(void *), void *arg) {
+    this->body = body;
+    this->arg = arg;
+}
+
+Thread::~Thread() {
+    // how to deallocate the stack
+    mem_free(myHandle);
+}
+
+int Thread::start() {
+    int error = thread_create(&myHandle, body, arg);
+    return error;
+}
+
+void Thread::join() {
+    thread_join(myHandle);
+}
+
+void Thread::dispatch() {
+    thread_dispatch();
+}
+
+int Thread::sleep(time_t time) {
+    return time_sleep(time);
+}
+
+Thread::Thread() {
+    body = threadRunWrapper;
+    arg = (void*) this;
+}
+
+void Thread::threadRunWrapper(void *arg) {
+    Thread *thread = (Thread*) arg;
+    thread->run();
+}
+
+Semaphore::Semaphore(unsigned int init) {
+    sem_open(&myHandle, init);
+}
+
+Semaphore::~Semaphore() {
+    sem_close(myHandle);
+}
+
+int Semaphore::wait() {
+    return sem_wait(myHandle);
+}
+
+int Semaphore::signal() {
+    return sem_signal(myHandle);
+}
+
+void PeriodicThread::terminate() {
+    period = 0;
+}
+
+PeriodicThread::PeriodicThread(time_t period) {
+    this->period = period;
+}
+
+void PeriodicThread::run() {
+    while (period > 0) {
+        periodicActivation();
+        Thread::sleep(period);
+    }
+}
+
+char Console::getc() {
+    return ::getc();
+}
+
+void Console::putc(char c) {
+    ::putc(c);
+}
